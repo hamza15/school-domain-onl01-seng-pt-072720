591fd10b7dbc292ce2e618c613bd70c4803c24ba
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-13 22:57:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,14 @@
 # code here!
 class School
   
+  
   def initialize(roster = {})
     @roster = roster
   end
   
   def add_student(name, grade)
-  
+    roster[name] = []
+    roster[name] << grade
   end
     
   
